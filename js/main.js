d6b9eab7094023e29aa9f2a6c0c0529b979c916a
Marszal <proconqpa@gmail.com>
2020-05-04 11:41:06 +0200

    main, about, pellet, contact without photos and map

---
+++ js/main.js
@@ -0,0 +1,6 @@
+// Links for menu elements
+
+document.querySelector("div.about").setAttribute('onclick', 'location.href = "about.html"');
+document.querySelector("div.pellet").setAttribute('onclick', 'location.href = "pellet.html"');
+document.querySelector("div.production").setAttribute('onclick', 'location.href = "production.html"');
+document.querySelector("div.contact").setAttribute('onclick', 'location.href = "contact.html"');
